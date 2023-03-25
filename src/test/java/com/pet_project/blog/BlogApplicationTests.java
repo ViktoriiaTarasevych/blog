dce1054e44980627d965d891b33b447a65246c25
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-03-25 20:26:53 +0200

    push project

---
+++ src/test/java/com/pet_project/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_project.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
