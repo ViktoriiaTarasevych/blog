1149823e75bef5f795cdbdc3c62e81ba8624fa0d
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-03-28 19:47:48 +0300

    create file blog-add.html

--- src/main/java/com/pet_project/blog/controllers/BlogControllers.java
+++ src/main/java/com/pet_project/blog/controllers/BlogControllers.java
@@ -1,5 +1,8 @@
 package com.pet_project.blog.controllers;
 
+import com.pet_project.blog.models.Post;
+import com.pet_project.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BlogControllers {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping ("/blog")
     public String blogMain (Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
+    }
+
+    @GetMapping ("/blog/add")
+    public String blogAdd (Model model) {
+        return "blog-add";
     }
 
 }
