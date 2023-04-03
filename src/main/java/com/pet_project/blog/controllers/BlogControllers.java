6b4c411ed5d0c944e707120b9263d3704d48ac11
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-04-03 20:02:10 +0300

    create blog-edit.html

--- src/main/java/com/pet_project/blog/controllers/BlogControllers.java
+++ src/main/java/com/pet_project/blog/controllers/BlogControllers.java
@@ -41,10 +41,36 @@ public class BlogControllers {
 
     @GetMapping("/blog/{id}") // вивод потрібного посту
     public String blogDetails(@PathVariable(value = "id") Long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
         Optional<Post> post = postRepository.findById(id);
         ArrayList<Post> result = new ArrayList<>(); // щоб було простіше перекидуємо в arraylist все
         post.ifPresent(result::add);
         model.addAttribute("post", result);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit") // редагування
+    public String blogEdit(@PathVariable(value = "id") Long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit") // додавання поста
+    public String blogPostUpdate(@PathVariable(value = "id") Long id, @RequestParam String title, @RequestParam String anons,
+                              @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
